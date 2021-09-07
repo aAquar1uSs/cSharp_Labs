66d159c76348a330cacdfb2e70fccbd4278e927b
Oleksandr <Panchenko.Sasha.nick74@gmail.com>
2021-09-07 16:56:18 +0300

    Lab3

---
+++ LabNumber3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber3
+{
+    class Program
+    {
+        private int x1;
+        private int x2;
+        private int y1;
+        private int y2;
+
+
+        public Program()
+        {
+            x1 = 0;
+            x2 = 0;
+            y1 = 0;
+            y2 = 0;
+        }
+
+        public int getX1()
+        {
+            return x1;
+        }
+
+        public void setX1(int value)
+        {
+            this.x1 = value;
+        }
+
+        public int getX2()
+        {
+            return x2;
+        }
+
+        public void setX2(int value)
+        {
+            this.x2 = value;
+        }
+
+        public int getY1()
+        {
+            return y1;
+        }
+
+        public void setY1(int value)
+        {
+            this.y1 = value;
+        }
+
+        public int getY2()
+        {
+            return y2;
+        }
+
+        public void setY2(int value)
+        {
+            this.y2 = value;
+        }
+
+        public bool calculate()
+        {
+            //получаем модуль числа
+            if (Math.Abs(getX1() - getX2()) <= 1 && Math.Abs(getY1() - getY2()) <= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void writeAndReadConsole()
+        {
+            Console.WriteLine("Set the coordinates for the first board.");
+            Console.Write("X1 =");
+            setX1(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Y1 =");
+            setY1(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Set the coordinates for the second board.");
+            Console.Write("X2 =");
+            setX2(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Y2 =");
+            setY2(Convert.ToInt32(Console.ReadLine()));
+        }
+
+
+        static void Main(string[] args)
+        {
+         
+            Program p = new Program();
+            p.writeAndReadConsole();
+
+            Console.WriteLine(p.calculate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabNumber3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LabNumber3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LabNumber3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("711de4d5-85df-47d4-a995-84b4d520b05e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
